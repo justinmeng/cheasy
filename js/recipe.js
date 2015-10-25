a83fb7ceb60f8dd56188dcbf115cc9cc91b066e1
Shacklez <kevanhn@uci.edu>
2015-10-24 23:39:13 -0700

    Added the Recipes html and fixed organization in other files

---
+++ js/recipe.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+	// $(".category-button").click(function() {
+	// 	alert("lol");
+	// 	$(this).attr("href", function(index, value) {
+	// 		return value + "?category=temp";
+	// 	});
+	//
+	// });
+
+});
+
+
